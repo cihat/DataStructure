1c2fddd75e04c64e24e353706784a811221f9aa3
cihatdev <cihat02020202@gmail.com>
2020-10-15 15:00:15 +0300

    started lesson1

--- LinkedList/Lesson-1/src/Main.java
+++ LinkedList/Lesson-1/src/Main.java
@@ -1,6 +1,18 @@
+
+class Eleman {
+  int icerik;
+  Eleman ileri;
+
+  Eleman(int icerik) {
+    this.icerik = icerik;
+    ileri = null;
+  }
+}
+
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
   }
 }
