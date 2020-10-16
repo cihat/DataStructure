193b55a92223b9ceca137cd2665d2509600e0cef
cihatdev <cihat02020202@gmail.com>
2020-10-16 14:53:24 +0300

    added vscode in .gitignore

--- LinkedList/Lesson-3/src/App.java
+++ LinkedList/Lesson-3/src/App.java
@@ -11,6 +11,7 @@ public class App {
     }
   }
 
+  // Bir dizinin k'ıncı yerine yeni eleman ekleyen algoritma
   static void diziyeEkle(int dizi[], int k, int yeni) {
     int i;
     for (i = dizi.length - 2; i >= k; i--) {
@@ -22,12 +23,34 @@ public class App {
     }
   }
 
+  // Sıralı bir dizide ikili arama yöntemiyle k sayısını arayan algoritma
+  static int dizideAra(int[] dizi, int k) {
+    int sol = 0, sag = dizi.length - 1, orta;
+    orta = (sol + sag) / 2;
+    while (sol <= sag) {
+      if (k < dizi[orta]) {
+        sag = orta - 1;
+      } else {
+        if (k > dizi[orta]) {
+          sol = orta + 1;
+        } else {
+          return orta;
+        }
+      }
+      orta = (sol + sag) / 2;
+    }
+    return -1;
+  }
+
   public static void main(String[] args) {
-    // // Bir dizinin k'nıncı elemanını sile algoritma
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    // Bir dizinin k'nıncı elemanını sile algoritma
     // dizidenSil(arr, 3);
 
     // Bir dizinin k'ıncı yerine yeni eleman ekleyen algoritma
-    diziyeEkle(arr, 5, 90);
+    // diziyeEkle(arr, 5, 90);
+
+    // Sıralı bir dizide ikili arama yöntemiyle k sayısını arayan algoritma
+    System.out.println(dizideAra(arr, 5));
   }
 }
